749d7bff600a15dfa61dd7e6685e8922b30a3eef
yusea <yuxin@mail.ustc.edu.cn>
2019-02-15 18:37:57 -0500

    mdify

--- testmemory.c
+++ testmemory.c
@@ -16,19 +16,19 @@ int main(int argc, char* argv[])
         if(fork() == 0){
             test = (char*)shmem_access(0);
             int count1 = shmem_count(0);
-            printf(1, "share memory index 0 have %d number process in use\n", count1);
+            printf(1, "share memory index 0, %d number process in use\n", count1);
             *test = 'b';
             *x = 'c';
-            printf(1, "son1: test addr:%x, test value:%c, x addr:%x, x value:%c\n", test, *test, x, *x);
+            printf(1, "test sub A: test addr:%x, test value:%c, x addr:%x, x value:%c\n", test, *test, x, *x);
         } else{
             wait();
             //for(int i = 0; i < 100000;i++);
             test = (char*)shmem_access(0);
             int count2 = shmem_count(0);
-            printf(1, "share memory index 0 have %d number process in use\n", count2);
+            printf(1, "share memory index 0, %d number process in use\n", count2);
             *test = 'd';
             //	test = (int*)p;
-            printf(1, "son2: test addr:%x, test value:%c, x addr:%x, x value:%c\n", test, *test, x, *x);
+            printf(1, "test sub B: test addr:%x, test value:%c, x addr:%x, x value:%c\n", test, *test, x, *x);
 
         }
     }
@@ -38,9 +38,9 @@ int main(int argc, char* argv[])
         //for(int i = 0; i < 100000;i++);
         test = (char*)shmem_access(0);
         int count3 = shmem_count(0);
-        printf(1, "share memory index 0 have %d number process in use\n", count3);
+        printf(1, "share memory index 0, %d number process in use\n", count3);
         // test = (int*)p;
-        printf(1, "parent: test addr:%x, test value:%c, x addr:%x, x value:%c\n", test, *test, x, *x);
+        printf(1, "test origin: test addr:%x, test value:%c, x addr:%x, x value:%c\n", test, *test, x, *x);
 
     }
     printf(1,"pid:%d\n", pid);
